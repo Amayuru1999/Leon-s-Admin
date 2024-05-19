470f163bd6c45d016aab242a0e39a02be00c5cca
Amayuru1999 <amarasingheau@gmail.com>
2024-05-19 13:41:52 +0530

    Initial Commit

---
+++ src/dummyData.js
@@ -0,0 +1,231 @@
+export const userData = [
+  {
+    month: "Jan",
+    sales: 4000,
+  },
+  {
+    name: "Feb",
+    sales: 3000,
+  },
+  {
+    name: "Mar",
+    sales: 5000,
+  },
+  {
+    name: "Apr",
+    sales: 4000,
+  },
+  {
+    name: "May",
+    sales: 3000,
+  },
+  {
+    name: "Jun",
+    sales: 2000,
+  },
+  {
+    name: "Jul",
+    sales: 4000,
+  },
+  {
+    name: "Agu",
+    sales: 3000,
+  },
+  {
+    name: "Sep",
+    sales: 4000,
+  },
+  {
+    name: "Oct",
+    sales: 1000,
+  },
+  {
+    name: "Nov",
+    sales: 4000,
+  },
+  {
+    name: "Dec",
+    sales: 3000,
+  },
+];
+
+export const productData = [
+  {
+    name: "Jan",
+    Sales: 4000,
+  },
+  {
+    name: "Feb",
+    Sales: 3000,
+  },
+  {
+    name: "Mar",
+    Sales: 5000,
+  },
+];
+
+export const userRows = [
+  {
+    id: 1,
+    username: "Gihan Sanjaya",
+    avatar: "/images/users/q1.jpeg",
+    email: "usertest1@gmail.com",
+    status: "active",
+    transaction: "LKR 12000.00",
+  },
+  {
+    id: 2,
+    username: "Saman Sirisena",
+    avatar: "/images/users/q2.jpeg",
+    email: "usertest1@gmail.com",
+    status: "active",
+    transaction: "LKR 12000.00",
+  },
+  {
+    id: 3,
+    username: "Viraj de Silva",
+    avatar: "/images/users/q3.jpeg",
+    email: "usertest1@gmail.com",
+    status: "active",
+    transaction: "LKR 12000.00",
+  },
+  {
+    id: 4,
+    username: "Saman Kumara",
+    avatar: "/images/users/q4.jpeg",
+    email: "usertest1@gmail.com",
+    status: "active",
+    transaction: "LKR 12000.00",
+  },
+  {
+    id: 5,
+    username: "Vimal Samarasinghe",
+    avatar: "/images/users/q5.jpeg",
+    email: "usertest1@gmail.com",
+    status: "active",
+    transaction: "LKR 12000.00",
+  },
+  {
+    id: 6,
+    username: "Naduni Kaushalya",
+    avatar: "/images/users/q6.jpeg",
+    email: "usertest1@gmail.com",
+    status: "active",
+    transaction: "LKR 12000.00",
+  },
+  {
+    id: 7,
+    username: "Hiruna Dickson",
+    avatar: "/images/users/q2.jpeg",
+    email: "usertest1@gmail.com",
+    status: "active",
+    transaction: "LKR 12000.00",
+  },
+  {
+    id: 8,
+    username: "Praveen Madhawa",
+    avatar: "/images/users/q1.jpeg",
+    email: "usertest1@gmail.com",
+    status: "active",
+    transaction: "LKR 12000.00",
+  },
+  {
+    id: 9,
+    username: "Nadee Ranaweera",
+    avatar: "/images/users/q7.jpeg",
+    email: "usertest1@gmail.com",
+    status: "active",
+    transaction: "LKR 12000.00",
+  },
+  {
+    id: 10,
+    username: "Madhara Subasinghe",
+    avatar: "/images/users/q8.jpeg",
+    email: "usertest1@gmail.com",
+    status: "active",
+    transaction: "LKR 12000.00",
+  },
+];
+
+export const productRows = [
+  {
+    id: 1,
+    name: "Butter Chicken",
+    img: "/images/foods/1.jpeg",
+    stock: 123,
+    status: "active",
+    price: "Rs.120.00",
+  },
+  {
+    id: 2,
+    name: "Palak Paneer",
+    img: "/images/foods/2.jpeg",
+    stock: 123,
+    status: "active",
+    price: "Rs.120.00",
+  },
+  {
+    id: 3,
+    name: "Chole Bhature",
+    img: "/images/foods/3.jpeg",
+    stock: 123,
+    status: "active",
+    price: "Rs.120.00",
+  },
+  {
+    id: 4,
+    name: "Biryani",
+    img: "/images/foods/4.jpeg",
+    stock: 123,
+    status: "active",
+    price: "Rs.120.00",
+  },
+  {
+    id: 5,
+    name: "Masala Dosa",
+    img: "/images/foods/5.jpeg",
+    stock: 123,
+    status: "active",
+    price: "Rs.120.00",
+  },
+  {
+    id: 6,
+    name: "Samosa",
+    img: "/images/foods/1.jpeg",
+    stock: 123,
+    status: "active",
+    price: "Rs.120.00",
+  },
+  {
+    id: 7,
+    name: "Rogan Josh",
+    img: "/images/foods/2.jpeg",
+    stock: 123,
+    status: "active",
+    price: "Rs.120.00",
+  },
+  {
+    id: 8,
+    name: "Pav Bhaji",
+    img: "/images/foods/3.jpeg",
+    stock: 123,
+    status: "active",
+    price: "Rs.120.00",
+  },
+  {
+    id: 9,
+    name: "Gulab Jamun",
+    img: "/images/foods/4.jpeg",
+    stock: 123,
+    status: "active",
+    price: "Rs.120.00",
+  },
+  {
+    id: 10,
+    name: "Rasgulla",
+    img: "/images/foods/5.jpeg",
+    stock: 123,
+    status: "active",
+    price: "Rs.120.00",
+  },
+];
